394bbf235f7102a4d2851c82ddffe0f4ddcbcfc9
Steve Griffith <steve.griffith@microsoft.com>
2018-04-13 11:17:20 -0400

    modified footer

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt2
+        Azure Global Blackbelt Dudes
       </div>
       <div class="col-lg-6">
       </div>
